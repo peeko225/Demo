18549652f09ac5c9e41384a240039062d1ca0bd0
peeko225 <qianneil233@gmail.com>
2025-09-08 21:28:01 -0400

    Update Main.java

--- Practice/src/Main.java
+++ Practice/src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 236; i++) {
             System.out.println("welcome to csc236!");
+            System.out.println("welcome to csc236!");
         }
-    }//?
+    }
+    
 }
